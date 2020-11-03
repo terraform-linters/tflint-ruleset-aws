7a378e2894fab01f3a2d20919aa832b18116b7b4
wata_mac <watassbass@gmail.com>
2020-11-03 18:18:48 +0900

    Serve custom ruleset for using custom config/runner

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/terraform-linters/tflint-ruleset-aws
 
-go 1.13
+go 1.15
 
 require (
-	github.com/hashicorp/hcl/v2 v2.2.0
-	github.com/terraform-linters/tflint-plugin-sdk v0.1.0
+	github.com/hashicorp/hcl/v2 v2.7.0
+	github.com/terraform-linters/tflint-plugin-sdk v0.5.1-0.20201031131801-2f5c943768d5
 )
--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"github.com/terraform-linters/tflint-ruleset-aws/rules"
 	"github.com/terraform-linters/tflint-plugin-sdk/plugin"
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
+	"github.com/terraform-linters/tflint-ruleset-aws/aws"
+	"github.com/terraform-linters/tflint-ruleset-aws/rules"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		RuleSet: tflint.RuleSet{
-			Name:    "aws",
-			Version: "0.1.0",
-			Rules: []tflint.Rule{
-				rules.NewAwsInstanceExampleTypeRule(),
+		RuleSet: &aws.RuleSet{
+			BuiltinRuleSet: tflint.BuiltinRuleSet{
+				Name:    "aws",
+				Version: "0.1.0",
+				Rules: []tflint.Rule{
+					rules.NewAwsInstanceExampleTypeRule(),
+					rules.NewAwsCustomRunnerRule(),
+				},
 			},
 		},
 	})
