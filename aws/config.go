ff6503bd5ba4ebae3f133b17b35f4fe4545a7363
kaito3desuyo <admin@seapolis.me>
2022-12-02 14:13:42 +0900

    Add assume role configuration in Config struct

--- aws/config.go
+++ aws/config.go
@@ -8,14 +8,22 @@ type Config struct {
 	Region                string `hclext:"region,optional"`
 	Profile               string `hclext:"profile,optional"`
 	SharedCredentialsFile string `hclext:"shared_credentials_file,optional"`
+	AssumeRoleARN         string `hclext:"assume_role_arn,optional"`
+	AssumeRoleExternalID  string `hclext:"assume_role_external_id,optional"`
+	AssumeRolePolicy      string `hclext:"assume_role_policy,optional"`
+	AssumeRoleSessionName string `hclext:"assume_role_session_name,optional"`
 }
 
 func (c *Config) toCredentials() Credentials {
 	return Credentials{
-		AccessKey: c.AccessKey,
-		SecretKey: c.SecretKey,
-		Region:    c.Region,
-		Profile:   c.Profile,
-		CredsFile: c.SharedCredentialsFile,
+		AccessKey: 				c.AccessKey,
+		SecretKey: 				c.SecretKey,
+		Region:    				c.Region,
+		Profile:   				c.Profile,
+		CredsFile: 				c.SharedCredentialsFile,
+		AssumeRoleARN: 			c.AssumeRoleARN,
+		AssumeRoleExternalID: 	c.AssumeRoleExternalID,
+		AssumeRolePolicy: 		c.AssumeRolePolicy,
+		AssumeRoleSessionName: 	c.AssumeRoleSessionName,
 	}
 }
