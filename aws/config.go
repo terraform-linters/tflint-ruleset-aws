c79463eec4ca011c1119bcd44a673758f502e250
kaito3desuyo <admin@seapolis.me>
2022-12-04 23:45:57 +0900

    Change to declare `assume_role` block like a Terraform

--- aws/config.go
+++ aws/config.go
@@ -1,29 +1,38 @@
 package aws
 
+type AssumeRole struct {
+	RoleARN     string `hclext:"role_arn,optional"`
+	ExternalID  string `hclext:"external_id,optional"`
+	Policy      string `hclext:"policy,optional"`
+	SessionName string `hclext:"session_name,optional"`
+}
+
 // Config is the configuration for the ruleset.
 type Config struct {
-	DeepCheck             bool   `hclext:"deep_check,optional"`
-	AccessKey             string `hclext:"access_key,optional"`
-	SecretKey             string `hclext:"secret_key,optional"`
-	Region                string `hclext:"region,optional"`
-	Profile               string `hclext:"profile,optional"`
-	SharedCredentialsFile string `hclext:"shared_credentials_file,optional"`
-	AssumeRoleARN         string `hclext:"assume_role_arn,optional"`
-	AssumeRoleExternalID  string `hclext:"assume_role_external_id,optional"`
-	AssumeRolePolicy      string `hclext:"assume_role_policy,optional"`
-	AssumeRoleSessionName string `hclext:"assume_role_session_name,optional"`
+	DeepCheck              bool       `hclext:"deep_check,optional"`
+	AccessKey              string     `hclext:"access_key,optional"`
+	SecretKey              string     `hclext:"secret_key,optional"`
+	Region                 string     `hclext:"region,optional"`
+	Profile                string     `hclext:"profile,optional"`
+	SharedCredentialsFile  string     `hclext:"shared_credentials_file,optional"`
+	AssumeRole            *AssumeRole `hclext:"assume_role,block"`
 }
 
 func (c *Config) toCredentials() Credentials {
-	return Credentials{
-		AccessKey:             c.AccessKey,
-		SecretKey:             c.SecretKey,
-		Region:                c.Region,
-		Profile:               c.Profile,
-		CredsFile:             c.SharedCredentialsFile,
-		AssumeRoleARN:         c.AssumeRoleARN,
-		AssumeRoleExternalID:  c.AssumeRoleExternalID,
-		AssumeRolePolicy:      c.AssumeRolePolicy,
-		AssumeRoleSessionName: c.AssumeRoleSessionName,
+	credentials := Credentials{
+		AccessKey: c.AccessKey,
+		SecretKey: c.SecretKey,
+		Region:    c.Region,
+		Profile:   c.Profile,
+		CredsFile: c.SharedCredentialsFile,
 	}
+
+	if c.AssumeRole != nil {
+		credentials.AssumeRoleARN         = c.AssumeRole.RoleARN
+		credentials.AssumeRoleExternalID  = c.AssumeRole.ExternalID
+		credentials.AssumeRolePolicy      = c.AssumeRole.Policy
+		credentials.AssumeRoleSessionName = c.AssumeRole.SessionName
+	}
+
+	return credentials
 }
