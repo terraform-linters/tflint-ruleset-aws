c49380fe0271c88f53860bae8d93ca9db17dc83c
Marina Limeira <marina@gruntwork.io>
2023-02-03 15:28:25 +0100

    Remove CIDR block rule -- released in another repo

--- rules/preset.go
+++ rules/preset.go
@@ -3,7 +3,8 @@ package rules
 import "github.com/terraform-linters/tflint-plugin-sdk/tflint"
 
 var PresetRules = map[string][]tflint.Rule{
-	"cis": {
-		NewAwsSecurityGroupRuleInvalidCidrBlockRule(),
+	"s3": {
+		NewAwsS3BucketNameRule(),
+		NewAwsS3BucketInvalidACLRule(),
 	},
 }
