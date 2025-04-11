68e8939fe22cb4f151e44ae11aaad81a79cb1e96
aristosvo <8375124+aristosvo@users.noreply.github.com>
2025-04-12 01:05:48 +0300

    feat: update for auto gen

---
+++ rules/aws_write_only_arguments_test.go
@@ -0,0 +1,231 @@
+package rules
+
+import (
+	"testing"
+
+	"github.com/terraform-linters/tflint-plugin-sdk/helper"
+)
+
+func Test_AwsWriteOnlyAttribute(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Content  string
+		Expected helper.Issues
+		Fixed    string
+	}{
+		{
+			Name: "basic aws_db_instance",
+			Content: `
+resource "aws_db_instance" "test" {
+  password = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"password" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "password_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_db_instance" "test" {
+  password_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_db_instance",
+			Content: `
+resource "aws_db_instance" "test" {
+  password_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "basic aws_docdb_cluster",
+			Content: `
+resource "aws_docdb_cluster" "test" {
+  master_password = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"master_password" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "master_password_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_docdb_cluster" "test" {
+  master_password_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_docdb_cluster",
+			Content: `
+resource "aws_docdb_cluster" "test" {
+  master_password_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "basic aws_rds_cluster",
+			Content: `
+resource "aws_rds_cluster" "test" {
+  master_password = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"master_password" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "master_password_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_rds_cluster" "test" {
+  master_password_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_rds_cluster",
+			Content: `
+resource "aws_rds_cluster" "test" {
+  master_password_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "basic aws_redshift_cluster",
+			Content: `
+resource "aws_redshift_cluster" "test" {
+  master_password = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"master_password" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "master_password_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_redshift_cluster" "test" {
+  master_password_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_redshift_cluster",
+			Content: `
+resource "aws_redshift_cluster" "test" {
+  master_password_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "basic aws_redshiftserverless_namespace",
+			Content: `
+resource "aws_redshiftserverless_namespace" "test" {
+  admin_user_password = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"admin_user_password" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "admin_user_password_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_redshiftserverless_namespace" "test" {
+  admin_user_password_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_redshiftserverless_namespace",
+			Content: `
+resource "aws_redshiftserverless_namespace" "test" {
+  admin_user_password_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "basic aws_secretsmanager_secret_version",
+			Content: `
+resource "aws_secretsmanager_secret_version" "test" {
+  secret_string = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"secret_string" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "secret_string_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_secretsmanager_secret_version" "test" {
+  secret_string_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_secretsmanager_secret_version",
+			Content: `
+resource "aws_secretsmanager_secret_version" "test" {
+  secret_string_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+		{
+			Name: "basic aws_ssm_parameter",
+			Content: `
+resource "aws_ssm_parameter" "test" {
+  value = "test"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewAwsWriteOnlyArgumentsRule(),
+					Message: `"value" is a non-ephemeral attribute, which means this secret is stored in state. Please use write-only argument "value_wo".`,
+				},
+			},
+			Fixed: `
+resource "aws_ssm_parameter" "test" {
+  value_wo = "test"
+}
+`,
+		},
+		{
+			Name: "everything is fine aws_ssm_parameter",
+			Content: `
+resource "aws_ssm_parameter" "test" {
+  value_wo = "test"
+}
+`,
+			Expected: helper.Issues{},
+		},
+	}
+
+	rule := NewAwsWriteOnlyArgumentsRule()
+
+	for _, tc := range cases {
+		filename := "resource.tf"
+		runner := helper.TestRunner(t, map[string]string{filename: tc.Content})
+
+		if err := rule.Check(runner); err != nil {
+			t.Fatalf("Unexpected error occurred: %s", err)
+		}
+		helper.AssertIssuesWithoutRange(t, tc.Expected, runner.Issues)
+
+		want := map[string]string{}
+		if tc.Fixed != "" {
+			want[filename] = tc.Fixed
+		}
+		helper.AssertChanges(t, want, runner.Changes())
+	}
+}
---
+++ rules/write_only/generator/main.go
@@ -0,0 +1,67 @@
+//go:build generators
+
+package main
+
+import (
+	"strings"
+
+	tfjson "github.com/hashicorp/terraform-json"
+	utils "github.com/terraform-linters/tflint-ruleset-aws/rules/generator-utils"
+)
+
+type writeOnlyArgument struct {
+	OriginalAttribute    string
+	WriteOnlyAlternative string
+}
+
+func main() {
+	awsProvider := utils.LoadProviderSchema("../../tools/provider-schema/schema.json")
+
+	resourcesWithWriteOnly := map[string][]writeOnlyArgument{}
+	// Iterate over all resources in the AWS provider schema
+	for resourceName, resource := range awsProvider.ResourceSchemas {
+		if arguments := writeOnlyArguments(resource); len(arguments) > 0 {
+			// gather sensitive attributes with write only argument alternatives
+			resourcesWithWriteOnly[resourceName] = findReplaceableAttribute(arguments, resource)
+		}
+	}
+
+	// Generate the rule file
+	utils.GenerateFile("../../rules/aws_write_only_arguments.go", "../../rules/write_only/rule.go.tmpl", resourcesWithWriteOnly)
+
+	// Generate the test file
+	utils.GenerateFile("../../rules/aws_write_only_arguments_test.go", "../../rules/write_only/rule_test.go.tmpl", resourcesWithWriteOnly)
+}
+
+func findReplaceableAttribute(arguments []string, resource *tfjson.Schema) []writeOnlyArgument {
+	writeOnlyArguments := []writeOnlyArgument{}
+
+	for _, argument := range arguments {
+		// Check if the argument ends with "_wo" and if the original attribute without "_wo" suffix exists in the resource schema
+		if attribute := strings.TrimSuffix(argument, "_wo"); strings.HasSuffix(argument, "_wo") && resource.Block.Attributes[attribute] != nil {
+			writeOnlyArguments = append(writeOnlyArguments, writeOnlyArgument{
+				OriginalAttribute:    attribute,
+				WriteOnlyAlternative: argument,
+			})
+		}
+	}
+
+	return writeOnlyArguments
+}
+
+func writeOnlyArguments(resource *tfjson.Schema) []string {
+	if resource == nil || resource.Block == nil {
+		return []string{}
+	}
+
+	writeOnlyArguments := []string{}
+
+	// Check if the resource has any write-only attributes
+	for name, attribute := range resource.Block.Attributes {
+		if attribute.WriteOnly {
+			writeOnlyArguments = append(writeOnlyArguments, name)
+		}
+	}
+
+	return writeOnlyArguments
+}
