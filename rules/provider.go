e051181b1b8c131c810121590555e9906d65d9a4
aristosvo <8375124+aristosvo@users.noreply.github.com>
2025-04-12 01:29:45 +0300

    refactor: moving files around + DO NOT EDIT comment

--- rules/provider.go
+++ rules/provider.go
@@ -4,6 +4,7 @@ import (
 	"github.com/terraform-linters/tflint-plugin-sdk/tflint"
 	"github.com/terraform-linters/tflint-ruleset-aws/rules/api"
 	"github.com/terraform-linters/tflint-ruleset-aws/rules/models"
+	"github.com/terraform-linters/tflint-ruleset-aws/rules/write_only"
 )
 
 var manualRules = []tflint.Rule{
@@ -44,7 +45,7 @@ var manualRules = []tflint.Rule{
 	NewAwsSecurityGroupInlineRulesRule(),
 	NewAwsSecurityGroupRuleDeprecatedRule(),
 	NewAwsIAMRoleDeprecatedPolicyAttributesRule(),
-	NewAwsWriteOnlyArgumentsRule(),
+	write_only.NewAwsWriteOnlyArgumentsRule(),
 }
 
 // Rules is a list of all rules
