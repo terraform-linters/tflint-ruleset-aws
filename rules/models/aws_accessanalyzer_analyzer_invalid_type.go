74b62743e48e9f13ab316968cab0896df5f45d30
Stefan Freitag <stefan.freitag@udo.edu>
2025-06-24 21:21:12 +0200

    Add missing AWS access analyzer types

--- rules/models/aws_accessanalyzer_analyzer_invalid_type.go
+++ rules/models/aws_accessanalyzer_analyzer_invalid_type.go
@@ -29,6 +29,8 @@ func NewAwsAccessanalyzerAnalyzerInvalidTypeRule() *AwsAccessanalyzerAnalyzerInv
 			"ORGANIZATION",
 			"ACCOUNT_UNUSED_ACCESS",
 			"ORGANIZATION_UNUSED_ACCESS",
+			"ACCOUNT_INTERNAL_ACCESS",
+			"ORGANIZATION_INTERNAL_ACCESS",
 		},
 	}
 }
@@ -72,7 +74,7 @@ func (r *AwsAccessanalyzerAnalyzerInvalidTypeRule) Check(runner tflint.Runner) e
 			continue
 		}
 
-		err := runner.EvaluateExpr(attribute.Expr, func (val string) error {
+		err := runner.EvaluateExpr(attribute.Expr, func(val string) error {
 			found := false
 			for _, item := range r.enum {
 				if item == val {
